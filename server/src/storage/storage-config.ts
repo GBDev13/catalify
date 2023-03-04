e8495d472888c6ddf3f39849ba80a04f3707f9b0
Gabriel Borges <contato@gbdev.me>
2023-03-04 20:24:32 -0300

    fix: private key

--- server/src/storage/storage-config.ts
+++ server/src/storage/storage-config.ts
@@ -1,6 +1,6 @@
 export const StorageConfig = {
   projectId: process.env.PROJECT_ID,
-  private_key: process.env.PRIVATE_KEY,
+  private_key: process.env.PRIVATE_KEY.split(String.raw`\n`).join('\n'),
   client_email: process.env.CLIENT_EMAIL,
   mediaBucket: process.env.STORAGE_MEDIA_BUCKET,
 };
