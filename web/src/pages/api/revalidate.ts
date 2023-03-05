5ba9bdc04300d6f37b5fb52f560f3576454f2164
Gabriel Borges <contato@gbdev.me>
2023-03-05 00:38:19 -0300

    change: revalidate cors

--- web/src/pages/api/revalidate.ts
+++ web/src/pages/api/revalidate.ts
@@ -1,21 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { path } = req.body
   res.setHeader("Access-Control-Allow-Origin", "https://app.catalify.com.br");
   res.setHeader("Access-Control-Allow-Methods", "POST");
-
-  if(req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' })
-  }
 
   if (req.body.secret !== process.env.NEXT_PUBLIC_REVALIDATE_TOKEN) {
     return res.status(401).json({ message: 'Invalid token' })
   }
 
   try {
-    await res.revalidate(req.body.path)
-    return res.json({ revalidated: true })
+    await res.revalidate(path)
+    
+    return res.status(200).json({ message: "OK" })
   } catch (err) {
-    return res.status(500).send('Error revalidating')
+    return res.status(500).send({
+      message: `Failed to revalidate ${path}`
+    })
   }
 }
