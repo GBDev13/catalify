46526cf09879a038dc82c77247211d2f8810d4d0
Gabriel Borges <contato@gbdev.me>
2023-03-05 00:55:20 -0300

    cors domain

--- web/src/pages/api/revalidate.ts
+++ web/src/pages/api/revalidate.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { path } = req.body
-  res.setHeader("Access-Control-Allow-Origin", "https://app.catalify.com.br");
+  res.setHeader("Access-Control-Allow-Origin", "https://catalify.com.br");
   res.setHeader("Access-Control-Allow-Methods", "POST");
 
   if (req.body.secret !== process.env.NEXT_PUBLIC_REVALIDATE_TOKEN) {
@@ -11,7 +11,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   try {
     await res.revalidate(path)
-    
+
     return res.status(200).json({ message: "OK" })
   } catch (err) {
     return res.status(500).send({
