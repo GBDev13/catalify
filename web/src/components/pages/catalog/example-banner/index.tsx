a45e2da07ee59d8b8f18e4dd409f0338bc363a15
Gabriel Borges <contato@gbdev.me>
2023-03-05 23:32:35 -0300

    fix: correct example banner href

--- web/src/components/pages/catalog/example-banner/index.tsx
+++ web/src/components/pages/catalog/example-banner/index.tsx
@@ -4,7 +4,7 @@ export const ExampleBanner = () => {
   return (
     <div className="w-full bg-primary text-readable min-h-max py-3 px-2 flex items-center justify-center text-xs text-center md:text-base">
       <span>
-        Esse catálogo é um exemplo, todo o conteúdo é fictício. Para criar o seu catálogo, acesse <Link href="/" className="underline transition-colors hover:text-indigo-500">catalify.com.br</Link>
+        Esse catálogo é um exemplo, todo o conteúdo é fictício. Para criar o seu catálogo, acesse <a href="https://catalify.com.br" className="underline transition-colors">catalify.com.br</a>
       </span>
     </div>
   )
